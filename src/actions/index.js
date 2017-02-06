c5743b2255727f974489f726526d25a47e559aab
mashnello <mashnenko@ukr.net>
2017-02-06 09:28:54 +0200

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const API_KEY = '0328c7e6cfb39b0531bb3294be6e9146';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
